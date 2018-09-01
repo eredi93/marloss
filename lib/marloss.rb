306e53f3d8d9aeec1d62f6a2e919351d1167dd70
Jacopo Scrinzi <scrinzi.jacopo@gmail.com>
2018-09-01 21:40:55 +0100

    add rubocop and fix any offences

--- lib/marloss.rb
+++ lib/marloss.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-#
+
 require "aws-sdk-dynamodb"
 
 require "marloss/version"
@@ -8,7 +8,6 @@ require "marloss/store"
 require "marloss/locker"
 
 module Marloss
-
   def self.logger
     @logger ||= ::Logger.new(STDOUT)
   end
@@ -19,10 +18,10 @@ module Marloss
 
   def self.included(base)
     base.define_singleton_method(:marloss_options) do |opts|
-      if opts[:table].nil?
-        raise(MissingParameterError, "DynamoDB Hash Key not set")
-      elsif opts[:hash_key].nil?
-        raise(MissingParameterError, "DynamoDB Table not set")
+      %i[table hash_key].each do |key|
+        next unless opts[key].nil?
+
+        raise(MissingParameterError, "DynamoDB #{key.to_s.capitalize.tr('_', ' ')} not set")
       end
 
       define_method(:marloss_options_hash) { opts }
@@ -34,13 +33,12 @@ module Marloss
   end
 
   module InstanceMethods
-
     def marloss_store
-      @marloss_store ||=begin
+      @marloss_store ||= begin
         table = marloss_options_hash[:table]
         hash_key = marloss_options_hash[:hash_key]
-        options = marloss_options_hash.reject do |k, v|
-          k == :table || k == :hash_key
+        options = marloss_options_hash.reject do |k, _|
+          %i[table hash_key].include?(k)
         end
 
         Store.new(table, hash_key, options)
@@ -60,7 +58,5 @@ module Marloss
 
       locker.release_lock
     end
-
   end
-
 end
--- lib/marloss/error.rb
+++ lib/marloss/error.rb
@@ -1,23 +1,15 @@
 # frozen_string_literal: true
-#
+
 module Marloss
+  class Error < StandardError; end
 
-  class Error < StandardError
-  end
+  class CreateTableError < Error; end
 
-  class CreateTableError < Error
-  end
+  class SetTableTtlError < Error; end
 
-  class SetTableTtlError < Error
-  end
+  class LockNotObtainedError < Error; end
 
-  class LockNotObtainedError < Error
-  end
+  class LockNotRefreshedError < Error; end
 
-  class LockNotRefreshedError < Error
-  end
-
-  class MissingParameterError < Error
-  end
-
+  class MissingParameterError < Error; end
 end
